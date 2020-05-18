ee13e88a78222b738e3f8dd3afe2baae86a4aa75
Tony <tonyrkovar@gmail.com>
2020-05-18 16:45:26 -0500

    chore: updated Database and created a backup schema for prisma.

---
+++ prisma/backupSchema.md
@@ -0,0 +1,68 @@
+generator client {
+provider = "prisma-client-js"
+}
+
+datasource db {
+provider = "postgresql"
+url = env("DATABASE_URL")
+}
+
+model Issue {
+id Int @default(autoincrement()) @id
+issueAuthorId Int
+roundtableId Int
+title String
+user User @relation(fields: [issueAuthorId], references: [id])
+roundtable Roundtable @relation(fields: [roundtableId], references: [id])
+questions Question[]
+}
+
+model Member {
+id Int @default(autoincrement()) @id
+roundtableId Int
+userId Int
+roundtable Roundtable @relation(fields: [roundtableId], references: [id])
+user User @relation(fields: [userId], references: [id])
+}
+
+model Question {
+authorId Int
+id Int @default(autoincrement()) @id
+issueId Int
+user User @relation(fields: [authorId], references: [id])
+issue Issue @relation(fields: [issueId], references: [id])
+responses Response[]
+}
+
+model Response {
+content String
+id Int @default(autoincrement()) @id
+questionAuthorId Int
+questionId Int
+user User @relation(fields: [questionAuthorId], references: [id])
+question Question @relation(fields: [questionId], references: [id])
+}
+
+model Roundtable {
+createdAt DateTime @default(now())
+description String?
+id Int @default(autoincrement()) @id
+ownerId Int
+roundtablename String
+user User @relation(fields: [ownerId], references: [id])
+issues Issue[]
+member Member[]
+}
+
+model User {
+createdAt DateTime @default(now())
+email String @unique
+id Int @default(autoincrement()) @id
+name String?
+password String?
+issues Issue[]
+member Member[]
+questions Question[]
+responses Response[]
+roundtables Roundtable[]
+}
--- prisma/schema.sql
+++ prisma/schema.sql
@@ -1,51 +1,52 @@
-CREATE TABLE "public"."Users"(
+CREATE TABLE "public"."User"(
     id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     "password" VARCHAR(255),
     "createdAt" TIMESTAMP NOT NULL DEFAULT now()
 );
 
-CREATE TABLE "public"."Roundtables"(
+CREATE TABLE "public"."Roundtable"(
     id SERIAL PRIMARY KEY NOT NULL,
     roundtableName VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
     "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
-    "owner" INTEGER NOT NULL,
-    FOREIGN KEY ("owner") REFERENCES "public"."Users"(id)
+    "ownerId" INTEGER NOT NULL,
+    FOREIGN KEY ("ownerId") REFERENCES "public"."User"(id)
 );
 
-CREATE TABLE "public"."Members"(
+CREATE TABLE "public"."Member"(
     id SERIAL PRIMARY KEY NOT NULL,
     "roundtableId" INTEGER NOT NULL,
-    FOREIGN KEY ("roundtableId") REFERENCES "public"."Roundtables"(id),
+    FOREIGN KEY ("roundtableId") REFERENCES "public"."Roundtable"(id),
     "userId" INTEGER NOT NULL,
-    FOREIGN KEY ("userId") REFERENCES "public"."Users"(id)
+    FOREIGN KEY ("userId") REFERENCES "public"."User"(id)
 );
 
-CREATE TABLE "public"."Issues"(
+CREATE TABLE "public"."Issue"(
     id SERIAL PRIMARY KEY NOT NULL,
     "roundtableId" INTEGER NOT NULL,
-    FOREIGN KEY ("roundtableId") REFERENCES "public"."Roundtables"(id),
+    FOREIGN KEY ("roundtableId") REFERENCES "public"."Roundtable"(id),
     title VARCHAR(255) NOT NULL,
-    author INTEGER NOT NULL,
-    FOREIGN KEY ("author") REFERENCES "public"."Users"(id)
+    "issueAuthorId" INTEGER NOT NULL,
+    FOREIGN KEY ("issueAuthorId") REFERENCES "public"."User"(id)
 );
 
 
-CREATE TABLE "public"."Questions"(
+CREATE TABLE "public"."Question"(
     id SERIAL PRIMARY KEY NOT NULL,
     "issueId" INTEGER NOT NULL,
-    FOREIGN KEY ("issueId") REFERENCES "public"."Issues"(id),
-    author INTEGER NOT NULL,
-    FOREIGN KEY ("author") REFERENCES "public"."Users"(id)
+    FOREIGN KEY ("issueId") REFERENCES "public"."Issue"(id),
+    "authorId" INTEGER NOT NULL,
+    FOREIGN KEY ("authorId") REFERENCES "public"."User"(id)
 );
 
-CREATE TABLE "public"."Responses"(
+CREATE TABLE "public"."Response"(
     id SERIAL PRIMARY KEY NOT NULL,
     "questionId" INTEGER NOT NULL,
-    FOREIGN KEY ("questionId") REFERENCES "public"."Questions"(id),
+    FOREIGN KEY ("questionId") REFERENCES "public"."Question"(id),
     content VARCHAR(255) NOT NULL,
-    author INTEGER NOT NULL,
-    FOREIGN KEY ("author") REFERENCES "public"."Users"(id)
+    "questionAuthorId" INTEGER NOT NULL,
+    FOREIGN KEY ("questionAuthorId") REFERENCES "public"."User"(id)
 );
 
