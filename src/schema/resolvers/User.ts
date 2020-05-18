df87f7f08917d8d82d0e9bc779ec73a0556dac9d
Tony <tonyrkovar@gmail.com>
2020-05-18 16:47:03 -0500

    feat: added resolver folder/files

---
+++ src/schema/resolvers/User.ts
@@ -0,0 +1,22 @@
+import { Context } from "../../context";
+
+/* This code will be used once my typeDefs are fleshed out*/
+
+// export const questions = (parent, args, ctx: Context) => {
+// 	return ctx.prisma.user
+// 		.findOne({
+// 			where: {
+// 				id: parent.id,
+// 			},
+// 		})
+// 		.questions();
+// };
+// export const issues = (parent, args, ctx: Context) => {
+// 	return ctx.prisma.user
+// 		.findOne({
+// 			where: {
+// 				id: parent.id,
+// 			},
+// 		})
+// 		.issues();
+// };
