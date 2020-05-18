789fbc35f34035f6750ec99741bf5ac42f5368b7
Tony <tonyrkovar@gmail.com>
2020-05-18 16:46:00 -0500

    chore: updated my server to be more modular

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from "apollo-server";
-import { schema } from "./schema";
 import { context } from "./context";
+import { schema } from "./schema";
 
 new ApolloServer({
 	schema,
