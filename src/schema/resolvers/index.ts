3e4be168dfd8512aff1168dd6c8b0c3eb68d7582
Tony <tonyrkovar@gmail.com>
2020-05-19 17:31:21 -0500

    feat: Added issue and question resolver file

--- src/schema/resolvers/index.ts
+++ src/schema/resolvers/index.ts
@@ -2,11 +2,14 @@ import * as User from "./User";
 import * as Query from "./Query";
 import * as Mutation from "./Mutation";
 import * as Roundtable from "./Roundtable";
+import * as Issue from "./Issue";
+import * as Question from "./Question";
 
 export const resolvers = {
 	User,
 	Roundtable,
+	Issue,
+	Question,
 	Query,
 	Mutation,
 };
-
