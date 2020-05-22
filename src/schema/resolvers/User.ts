72a648438d5ed03f53cfc196ad3909840d143676
Tony <tonyrkovar@gmail.com>
2020-05-21 21:52:28 -0500

    chore: added User query resolvers

--- src/schema/resolvers/User.ts
+++ src/schema/resolvers/User.ts
@@ -2,22 +2,42 @@ import { Context } from "../../context";
 
 /* This code will be used once my typeDefs are fleshed out*/
 
-// export const questions = (parent, args, ctx: Context) => {
-// 	return ctx.prisma.user
-// 		.findOne({
-// 			where: {
-// 				id: parent.id,
-// 			},
-// 		})
-// 		.questions();
-// };
+export const questions = (parent, args, ctx: Context) => {
+	return ctx.prisma.user
+		.findOne({
+			where: {
+				id: parent.id,
+			},
+		})
+		.questions();
+};
 
-// export const issues = (parent, args, ctx: Context) => {
-// 	return ctx.prisma.user
-// 		.findOne({
-// 			where: {
-// 				id: parent.id,
-// 			},
-// 		})
-// 		.issues();
-// };
+export const issues = (parent, args, ctx: Context) => {
+	return ctx.prisma.user
+		.findOne({
+			where: {
+				id: parent.id,
+			},
+		})
+		.issues();
+};
+
+export const memberOf = (parent, args, ctx: Context) => {
+	return ctx.prisma.user
+		.findOne({
+			where: {
+				id: parent.id,
+			},
+		})
+		.memberOf();
+};
+
+export const roundtables = (parent, _, ctx: Context) => {
+	return ctx.prisma.user
+		.findOne({
+			where: {
+				id: parent.id,
+			},
+		})
+		.roundtables();
+};
