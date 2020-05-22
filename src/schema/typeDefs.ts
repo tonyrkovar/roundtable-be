4b590e0ec1765b6d5fc9d2d8f7acf58ad1f94a03
Tony <tonyrkovar@gmail.com>
2020-05-21 20:59:59 -0500

    chore: Updated typeDefs after schema change

--- src/schema/typeDefs.ts
+++ src/schema/typeDefs.ts
@@ -22,53 +22,71 @@ export const typeDefs = `
 		responses: [Response]!
 		response: Response!
 
+		member: Member!
+		members: [Member!]!
 		membersByRTId(roundtableId: ID!): Member!
 	}
 
 	type Mutation {
-		createUser(email: String!, password: String): User!
+		createUser(data: UserInput): User!
 		deleteUser(id: ID): User!
-		updateUser(id: ID!, email: String, password: String): User!
+		updateUser(id: ID!, data: UserInput): User!
 
-		createRoundtable(roundtablename: String!, owner: ID!, description: String): Roundtable!
+		createRoundtable(roundtableName: String!, owner: ID!, description: String): Roundtable!
 		deleteRoundtable(id: ID!): Roundtable!
-		updateRoundtable(id: ID!, owner: ID, roundtablename: String, description: String): Roundtable!
+		updateRoundtable(id: ID!, owner: ID, roundtableName: String, description: String): Roundtable!
 
-		createIssue(issueAuthor: ID!, roundtable: ID!, title: String!): Issue!
+		createIssue(issueAuthor: ID!, roundtable: ID!, title: String!, prompt: String): Issue!
 		deleteIssue(id: ID!): Issue!
-		updateIssue(id: ID!, title: String): Issue!
+		updateIssue(id: ID!, title: String, prompt: String): Issue!
 
-		createQuestion(author: ID!, issue: ID!, prompt: String!): Question!
-		updateQuestion(id: ID!, prompt: String!): Question!
+		createQuestion(author: ID!, issue: ID!, question: String!): Question!
+		updateQuestion(id: ID!, question: String!): Question!
 		deleteQuestion(id: ID!): Question!
+
+		createResponse(content: String!, question: ID!, responseAuthor: ID!): Response!
+		updateResponse(content: String!, question: ID!, responseAuthor: ID!, responseId: ID!): Response!
+		deleteResponse(id: ID!): Response!
+
+		createMember(roundtable: ID!, user: ID!): Member!
 	}
 	
-	input UserCreateInput {
+	input UserInput {
 		email: String!
-		password: String!
+		password: String
+		firstName: String
+		lastName: String
+		userName: String
 	}
 
 	type User {
 		id: ID!
 		email: String!
+		firstName: String!
+		lastName: String!
+		userName: String!
 		password: String!
 		createdAt: String!
 		questions: [Question!]!
 		issues: [Issue!]!
+		roundtables: [Roundtable!]!
+		memberOf: [Member!]!
 	}
 	
 	type Roundtable {
 		id: ID!
-		roundtablename: String!
+		roundtableName: String!
 		owner: User!
 		createdAt: String!
 		description: String!
 		issues: [Issue!]!
+		members: [Member!]!
 	}
 
 	type Issue{
 		id: ID!
-		author: User!
+		issueAuthor: User!
+		prompt: String!
 		title: String!
 		roundtable: Roundtable!
 		questions: [Question]!
@@ -77,19 +95,21 @@ export const typeDefs = `
 	type Response{
 		id: ID!
 		content: String!
-		questionAuthor: User!
+		responseAuthor: User!
+		question: Question! 
 	}
 
 	type Question{
 		id: ID!
 		author: User!
-		prompt: String!
+		question: String!
 		issue: Issue!
-		response: [Response]!
+		responses: [Response]!
 	}
 
 	type Member{
+		id: ID!
 		roundtable: Roundtable!
-		user: [User!]! 
+		user: User!
 	}
 	`;
