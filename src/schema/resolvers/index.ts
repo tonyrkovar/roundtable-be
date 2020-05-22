0bade19cfd6343ecae3eb83a969a54521ff5d036
Tony <tonyrkovar@gmail.com>
2020-05-21 21:01:11 -0500

    feat: added response and member nested queries.

--- src/schema/resolvers/index.ts
+++ src/schema/resolvers/index.ts
@@ -4,12 +4,16 @@ import * as Mutation from "./Mutation";
 import * as Roundtable from "./Roundtable";
 import * as Issue from "./Issue";
 import * as Question from "./Question";
+import * as Response from "./Response";
+import * as Member from "./Member";
 
 export const resolvers = {
 	User,
 	Roundtable,
 	Issue,
 	Question,
+	Response,
+	Member,
 	Query,
 	Mutation,
 };
